c26fd0846937859282c7471595e5e2e465dc8b03
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:31:05 +0000

    CPU intensive now

--- Driver1.c
+++ Driver1.c
@@ -21,11 +21,21 @@ int main(int argc, char *argv[]) {
 		
 		
 		int i=0;
+		
+		int j=0;
+		int k=0;
 		struct sched_param  params;
 		params.sched_priority=atoi( argv[2]);
 		sched_setscheduler(getpid(),   5 , &params);
 		for(i=0; i<100; i++){
-		printf("Program 1 just ran for the %dth time\n", i);
+			printf("Program 1 just started execution for the %dth time\n", i);
+			for(j=0; j<100; j++){
+				for(k=0; k<100; k++){
+					j*k;
+				}
+			}
+			
+			printf("Program 1 just finished execution for the %dth time\n", i);
 			strcat(argv[1], "Program 1 just ran for the ");
 			argv[1]+=i;
 			strcat(argv[1], "th time.\n");
