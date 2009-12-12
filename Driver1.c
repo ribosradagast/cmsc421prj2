2094993f1ab1b33373807b5874ce73a812b397f8
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:09:39 +0000

    yay!

--- Driver1.c
+++ Driver1.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[]) {
 	
 	int i=0;
 	struct sched_param  params;
-	&params->sched_priority=42;
+	params.sched_priority=42;
 	sched_setscheduler(getpid(), argv[2], params);
 	for(i=0; i<100; i++){
-		str_concat(argv[1], "Program 1 just ran for the %dth time.", i);
+		strcat(argv[1], "Program 1 just ran for the "+i+"th time.");
 		sleep(1);
 	}
 
