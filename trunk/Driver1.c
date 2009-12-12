efbb526318de9f32fa53a67fdac7caf9d1cb121e
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:17:45 +0000

    Last Revision!

--- trunk/Driver1.c
+++ trunk/Driver1.c
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]) {
 	sched_setscheduler(getpid(), atoi( argv[2]), &params);
 	for(i=0; i<100; i++){
 		strcat(argv[1], "Program 1 just ran for the ");
-		strcat(argv[1], (atoi)i);
+		strcat(argv[1], atoi(i));
 		strcat(argv[1], "th time.\n");
 		
 		sleep(1);
 	}
-
+return 0;
 }
