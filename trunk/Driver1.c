9fceee3314fd34d9e0d98a725e2580a11a364567
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:23:00 +0000

    error checking added

--- trunk/Driver1.c
+++ trunk/Driver1.c
@@ -9,16 +9,29 @@
 */
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	struct sched_param  params;
-	params.sched_priority=42;
-	sched_setscheduler(getpid(), atoi( argv[2]), &params);
-	for(i=0; i<100; i++){
-		strcat(argv[1], "Program 1 just ran for the ");
-		argv[1]+=i;
-		strcat(argv[1], "th time.\n");
-		
-		sleep(1);
+	if ( argc != 3 ) /* argc should be 2 for correct execution */
+	{
+		/* We print argv[0] assuming it is the program name */
+		printf( "You should have specified a string to append to and an integer for bucket ID\n");
+		return -1;
 	}
-return 0;
+	else 
+	{
+
+		
+		
+		int i=0;
+		struct sched_param  params;
+		params.sched_priority=42;
+		sched_setscheduler(getpid(), atoi( argv[2]), &params);
+		for(i=0; i<100; i++){
+			strcat(argv[1], "Program 1 just ran for the ");
+			argv[1]+=i;
+			strcat(argv[1], "th time.\n");
+			
+			sleep(1);
+		}
+		return 0;
+	}
+	
 }
