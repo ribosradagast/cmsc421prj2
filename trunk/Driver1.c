99791d8ad032d0d4b7b25a5aee1cb4a8b368fe43
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:26:51 +0000

    Seeing if it works with an existing scheduler

--- trunk/Driver1.c
+++ trunk/Driver1.c
@@ -22,8 +22,8 @@ int main(int argc, char *argv[]) {
 		
 		int i=0;
 		struct sched_param  params;
-		params.sched_priority=42;
-		sched_setscheduler(getpid(), atoi( argv[2]), &params);
+		params.sched_priority=atoi( argv[2]);
+		sched_setscheduler(getpid(),   5 , &params);
 		for(i=0; i<100; i++){
 		printf("Program 1 just ran for the %dth time", i);
 			strcat(argv[1], "Program 1 just ran for the ");
