904f07bb9a25471abdea567494153d3570a481f3
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-12 08:05:53 +0000

    Yet another revision

--- Driver1.c
+++ Driver1.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sched.h>
+#include <strings.h>
 
 /*
 * parameters : address to buffer, bucket ID
 */
 int main(int argc, char *argv[]) {
-{
-int i=0;
-struct sched_param  params;
-params->sched_priority=42;
-sched_setscheduler(getpid(), argv[2], &params);
-for(i=0; i<100; i++){
-puts(argv[1], "Program 1 just ran for the %dth time.", i);
-sleep(1);
-}
+	
+	int i=0;
+	struct sched_param  params;
+	&params->sched_priority=42;
+	sched_setscheduler(getpid(), argv[2], params);
+	for(i=0; i<100; i++){
+		str_concat(argv[1], "Program 1 just ran for the %dth time.", i);
+		sleep(1);
+	}
 
 }
