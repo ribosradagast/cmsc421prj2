fe3b2bfc03e9e94ad4cd69a6b1f6214ef4f99046
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-13 01:30:55 +0000

    Formatting changes

--- Driver.c
+++ Driver.c
@@ -10,16 +10,20 @@
 int main() {
 
 	/* put process 1 and 2 in 1 bucket */
-	execlp("process1.out","process1.out", 1);
-	execlp("process2.out","process2.out", 1);
+	printf("Starting process 1 in bucket 1\n");
+	execlp("./process1.out","process1.out", 1);
+	printf("Starting process 2 in bucket 1\n");
+	execlp("./process2.out","process2.out", 1);
 
 	/* put process 3 in a new bucket (-1) */
-	execlp("process3.out","process3.out", -1);
+	printf("Starting process 3 in a new bucket\n");
+	execlp("./process3.out","process3.out", -1);
 
 	/* put process 4 in a new bucket (4) */
-	execlp("process4.out","process4.out", 4);
+	printf("Starting process 4 in bucket 4\n");
+	execlp("./process4.out","process4.out", 4);
 
-	printf("Main program has terminated");
+	printf("Main program has terminated\n");
 	return 0;
 }
 
--- Driver4.c
+++ Driver4.c
@@ -22,20 +22,18 @@ int main(int argc, char *argv[]) {
 		int j=0;
 		int k=0;
 		struct sched_param  params;
-		params.sched_priority=atoi( argv[2]);
+		params.sched_priority=atoi( argv[1]);
 		sched_setscheduler(getpid(),   6 , &params);
 		for(i=0; i<10; i++){
-			printf("Program 3 just started execution for the %dth time\n", i);
+			printf("Program 4 just started execution for the %dth time\n", i);
 			for(j=0; j<100000; j++){
 				for(k=0; k<1000; k++){
 					j*k;
 				}
 			}
 			
-			printf("Program 3 just finished execution for the %dth time\n", i);
-			argv[1]+=i;
-			strcat(argv[1], "th time.\n");
-			
+			printf("Program 4 just finished execution for the %dth time\n", i);
+						
 		}
 		return 0;
 	}
