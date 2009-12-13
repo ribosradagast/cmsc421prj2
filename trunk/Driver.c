d9ac0552f0cd535dc0ac77e51a4e67d094df89a5
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-13 01:47:05 +0000

    fork()!

--- trunk/Driver.c
+++ trunk/Driver.c
@@ -11,16 +11,20 @@ int main() {
 
 	/* put process 1 and 2 in 1 bucket */
 	printf("Starting process 1 in bucket 1\n");
+	fork();
 	execlp("./process1.out","./process1.out", "1", (char *) 0);
 	printf("Starting process 2 in bucket 1\n");
+	fork();
 	execlp("./process2.out","./process2.out", "1", (char *) 0);
 
 	/* put process 3 in a new bucket (-1) */
 	printf("Starting process 3 in a new bucket\n");
+	fork();
 	execlp("./process3.out","./process3.out", "-1", (char *) 0);
 
 	/* put process 4 in a new bucket (4) */
 	printf("Starting process 4 in bucket 4\n");
+	fork();
 	execlp("./process4.out","./process4.out", "4", (char *) 0);
 
 	printf("Main program has terminated\n");
