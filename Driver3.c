2d18df167429aef8f677b4f0ed110e28b969e6c9
justin.raabe <justin.raabe@6e75d742-d7d6-11de-adf9-3515c1a490bc>
2009-12-13 17:34:38 +0000

    Lots more changes

--- Driver3.c
+++ Driver3.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
 		params.sched_priority=atoi( argv[1]);
 		sched_setscheduler(getpid(),   6 , &params);
 		for(i=0; i<10; i++){
-			printk(KERN_ALERT "Program 3 just started execution for the %dth time\n", i);
+			/*printk(KERN_ALERT "Program 3 just started execution for the %dth time\n", i);*/
 			printf("Program 3 just started execution for the %dth time\n", i);
 			for(j=0; j<100000; j++){
 				for(k=0; k<5000; k++){
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 				}
 			}
 			
-			printk(KERN_ALERT "Program 3 just finished execution for the %dth time\n", i);
+			/*printk(KERN_ALERT "Program 3 just finished execution for the %dth time\n", i);*/
 			printf("Program 3 just finished execution for the %dth time\n", i);
 						
 		}
